a35c175715906b3471708e534331c0679047ee5e
uglyonlytoday <superjang3357@naver.com>
2022-04-09 20:21:24 +0900

    implement appbar area

--- lib/app/modules/main/page.dart
+++ lib/app/modules/main/page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
+  Widget _appbarArea() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      leading: Builder(
+        builder: (context) => IconButton(onPressed: () {}, icon: const Icon(PhosphorIcons.sliders)),
+      ),
+      title: ActionChip(
+        onPressed: () {},
+        label: const Text('디미고 헬스장', style: TextStyle(color: Colors.white, fontSize: 13)),
+        backgroundColor: const Color(0xFF051311),
+        side: const BorderSide(width: 1, color: Color.fromRGBO(255, 255, 255, 0.4)),
+      ),
+      actions: [
+        IconButton(onPressed: () {}, icon: const Icon(PhosphorIcons.arrowsOutSimple)),
+        IconButton(onPressed: () {}, icon: const Icon(PhosphorIcons.dotsThreeVerticalBold)),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('MainPage')),
-      body: Container(),
+      backgroundColor: const Color(0xFF051311),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _appbarArea(),
+          ],
+        ),
+      ),
     );
   }
 }
